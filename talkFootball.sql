a8dc09b2774dd7cf7f8930a6cade1826f0f7e0d8
betimtasholli <betimtasholli2@gmail.com>
2020-05-04 13:52:25 +0200

    permiresimi i databazes

--- talkFootball.sql
+++ talkFootball.sql
@@ -24,7 +24,33 @@ mbiemri varchar (25) not null,
 email varchar (30) not null,
 passwordi varchar (25) not null,
 dataELindjes date not null,
-gjinia boolean);
+gjinia boolean,
+primary key(perdoruesiId));
+
+create table ligat(
+ligaId integer auto_increment,
+emriILiges varchar (25) not null,
+shteti varchar (25) not null,
+primary key (ligaId));
+
+create table ekipet(
+ekipiId integer auto_increment,
+emriIEkipit varchar (25) not null,
+ligaId integer not null,
+primary key (ekipiId),
+foreign key (ligaId) references ligat(ligaId) on delete cascade on update cascade);
+
+create table piket(
+ligaId integer not null,
+ekipiId integer not null,
+lojratELuajtura integer not null,
+fitoret integer not null,
+barazimet integer not null,
+humbjet integer not null,
+gollDiferenca integer not null,
+piket integer not null,
+foreign key (ligaId) references ligat(ligaId) on delete cascade on update cascade,
+foreign key (ekipiId) references ekipet(ekipiId) on delete cascade on update cascade);
 
 
 
